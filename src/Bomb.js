28cb7141c270ced729326304fec3d5f805d33ccd
Elijah Samuels <elijahsamuels@gmail.com>
2021-03-18 13:41:43 -0600

    commit tree 1aa28597054d085c4df0f12d44771e516e32ef21

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,27 @@
-// your Bomb code here!
+import React, { Component} from 'react';
+// import Bomb from 'src/Bomb.js';
+
+export default class Bomb extends Component {
+
+	constructor(props) {
+		super()
+		this.state = {
+			secondsLeft: props.initialCount
+		}
+	}
+	
+	render() {
+		const kaBoom = this.state.secondsLeft === 0 ? "Boom!" : `${this.state.secondsLeft} seconds left before I go boom!`;
+		return (
+		<div>{ kaBoom }</div>
+		)
+	}
+
+}
+			// <h1>120 seconds left before I go boom!</h1>;
+		// return <h1>Boom!</h1>;
+
+	// this.state.secondsLeft = ImageSlider.props.initialCount
+
+	// if this.state.secondsLeft == 0 then "Boom!" : "120 seconds left before I go boom!"
+	
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,29 @@
-// your ImageSlider code here!
+import React, { Component} from 'react';
+// import ImageSlider from 'src/ImageSlider.js';
+
+
+
+export default class ImageSlider extends Component {
+	
+	constructor() {
+		super()
+		this.state = {
+			currentSlideIndex: 0
+		}
+	}
+
+	// increment = () => {
+	// 	const newCount = this.state.currentSlideIndex + 1
+	// 	this.setState({
+	// 		currentSlideIndex: newCount
+	// 	})
+	// }
+
+		render() {
+		  return <div>
+					<h1>I am on slide {this.state.currentSlideIndex}</h1>
+				</div>
+	  }
+	  
+
+}
